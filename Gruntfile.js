ffc80554f5446ed72177104ea9cbccdd95fe4772
Alex <alex@recordsonribs.com>
2015-01-06 12:30:14 +0000

    Add JSHint and whitespace listing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// All JS source files we want to check.
+var allSources = ['Gruntfile.js', 'karma.conf.js', 'app/**/*.js'];
+
 module.exports = function(grunt){
   // Use Just In Time Grunt to prevent loading all modules via grunt.loadNpmTask(*) on every run.
   require('jit-grunt')(grunt);
@@ -12,11 +15,32 @@ module.exports = function(grunt){
         singleRun: true
       },
       unit: {}
+    },
+
+    // Lint all files for JS style violations.
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+
+      all: allSources
+    },
+
+    // Lint for two spaces as indentation as opposed to tabs.
+    lintspaces: {
+      options: {
+        trailingspaces: true,
+        indentation: 'spaces',
+        spaces: 2
+      },
+      all: {
+        src: allSources
+      }
     }
   });
 
   // Run all tests.
-  grunt.registerTask('test', ['karma:unit']);
+  grunt.registerTask('test', ['lintspaces', 'jshint', 'karma:unit']);
 
   // Test and build application.
   grunt.registerTask('build', ['test']);
--- app/app_test.js
+++ app/app_test.js
@@ -3,5 +3,5 @@
 describe('testing tests work', function(){
   it('should test that truth is true', function(){
     expect(true).toBeTruthy();
-  })
+  });
 });
