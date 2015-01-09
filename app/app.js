75dcd4d044e8bf1bd321e505387ad0be0eced253
Alex <alex@recordsonribs.com>
2015-01-09 16:58:14 +0000

    Add Marvin controllers module.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('Marvin.Services', []);
+angular.module('Marvin.Controllers', []);
 
-angular.module('Marvin', ['Marvin.Services']);
+angular.module('Marvin', ['Marvin.Services', 'Marvin.Controllers']);
 
