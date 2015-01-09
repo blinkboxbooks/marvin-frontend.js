36bbcbf116eae2b3fafdfd26db6bd557e62356b8
Alex <alex@recordsonribs.com>
2015-01-09 11:19:52 +0000

    Implement basic IMS services.

--- app/services/ims-service.js
+++ app/services/ims-service.js
@@ -1,6 +1,34 @@
 'use strict';
 
 angular.module('Marvin.Services')
-  .factory('IMS', function(){
-    return {};
+  .factory('IMS', function($http){
+  var url = 'http://localhost';
+
+  /**
+   * Searches for a query from IMS.
+   *
+   * @param query A valid Lucene query against the database.
+   * @param count The number of results to be returned.
+   * @param offset The offset.
+   * @returns {HttpPromise}
+   */
+  function search(query, count, offset){
+    var paramString = [];
+
+    paramString.push('q=' + query);
+
+    if (angular.isDefined(count)){
+      paramString.push('count=' + count);
+    }
+
+    if (angular.isDefined(offset)){
+      paramString.push('offset=' + offset);
+    }
+
+    return $http.get(url + '/search?' + paramString.join('&'));
+  }
+
+  return {
+    search: search
+  };
 });
--- app/services/ims-service_test.js
+++ app/services/ims-service_test.js
@@ -1,17 +1,48 @@
 'use strict';
 
 describe('IMS service', function(){
+  var $httpBackend;
+
   var IMS;
 
   beforeEach(module('Marvin'));
 
-  beforeEach(inject(function(_IMS_){
+  beforeEach(inject(function(_IMS_, _$httpBackend_){
+    $httpBackend = _$httpBackend_;
     IMS = _IMS_;
   }));
 
   // Stupid test.
   it('returns an object', function(){
     expect(typeof(IMS)).toBe('object');
+  });
+
+  describe('search querying', function(){
+    it('there is a function', function(){
+      expect(typeof(IMS.search)).toBe('function');
+    });
+
+    it('should pass along a search string correctly', function(){
+      $httpBackend.expect('GET', 'http://localhost/search?q=hello').respond(200);
+
+      IMS.search('hello');
+      $httpBackend.flush();
+    });
+
+    it('should allow you set count for a number of requests', function(){
+      $httpBackend.expect('GET', 'http://localhost/search?q=blah&count=10').respond(200);
+
+      IMS.search('blah', 10);
+      $httpBackend.flush();
+    });
+
+    it('should allow one to set a offset for the request', function(){
+      $httpBackend.expect('GET', 'http://localhost/search?q=thing&count=10&offset=10').respond(200);
+
+      IMS.search('thing', 10, 10);
+
+      $httpBackend.flush();
+    });
   });
 });
 
