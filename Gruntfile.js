a17182d033759f8c43cefa9a13728b7d81c6c793
Alex <alex@recordsonribs.com>
2015-01-12 15:24:10 +0000

    This should be 0.0.0.0.

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,7 +83,7 @@ module.exports = function(grunt){
     connect: {
       options: {
         port: 8000,
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         protocol: 'http',
         base: './'
       },
