a3565fdbc9472e2ef526ae14e7ccee4dc218406f
Alex <alex@recordsonribs.com>
2015-01-14 15:16:44 +0000

    Move search into its own view, provide routing.

--- app/app.js
+++ app/app.js
@@ -3,5 +3,14 @@
 angular.module('Marvin.Services', []);
 angular.module('Marvin.Controllers', []);
 
-angular.module('Marvin', ['Marvin.Services', 'Marvin.Controllers', 'Marvin.Constants']);
+angular.module('Marvin', ['ngRoute', 'Marvin.Services', 'Marvin.Controllers', 'Marvin.Constants'])
+    .config(function($routeProvider, $locationProvider){
+      $routeProvider
+          .when('/', {
+            controller: 'SearchController',
+            templateUrl: 'app/search/search.html'
+          });
+
+      $locationProvider.html5Mode(true);
+    });
 
