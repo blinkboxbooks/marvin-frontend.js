2298475af9504977d7b437b2ce16175b75efe59e
Alex <alex@recordsonribs.com>
2015-01-16 17:44:25 +0000

    Basic pagination.

--- app/search/search-controller.js
+++ app/search/search-controller.js
@@ -4,6 +4,7 @@ angular.module('Marvin.Controllers')
   .controller('SearchController', function(IMS, $scope){
       $scope.resultsOpen = false;
       $scope.advancedSearchOpen = false;
+      $scope.notFirstPage = false;
 
       $scope.queryFromForm = '';
 
@@ -18,6 +19,11 @@ angular.module('Marvin.Controllers')
       };
 
       $scope.results = {};
+      $scope.totalResults = 0;
+      $scope.offset = 0;
+
+      $scope.showPager = false;
+
       $scope.errors = [];
 
       $scope.search = function(){
@@ -26,9 +32,39 @@ angular.module('Marvin.Controllers')
 
         IMS.search($scope.queryFromForm).then(function(result){
           $scope.results = result.data;
+
+          if (result.data.lastPage === false) {
+            $scope.showPager = true;
+          }
+
+          $scope.totalResults = $scope.results.items.length;
         },
         function(){
           $scope.errors.push({message: 'Something went sadly wrong.'});
+        });
+      };
+
+     $scope.nextPage = function(){
+      $scope.offset = angular.copy($scope.totalResults);
+
+       IMS.search($scope.queryFromForm, 50, $scope.offset).then(function(result){
+         $scope.results = result.data;
+
+         $scope.totalResults += $scope.results.items.length;
+
+         $scope.notFirstPage = true;
+       });
+     };
+
+      $scope.previousPage = function(){
+        $scope.offset -= 50;
+
+        IMS.search($scope.queryFromForm, 50, $scope.offset).then(function(result){
+          $scope.results = result.data;
+
+          $scope.totalResults -= 50;
+
+          $scope.notFirstPage = true;
         });
       };
 });
--- app/search/search-controller_test.js
+++ app/search/search-controller_test.js
@@ -70,99 +70,176 @@ describe('Search controller', function(){
     });
   });
 
-  it('calls the IMS service', function(){
-    scope.queryFromForm = 'hello world';
-    var deferred = $q.defer();
+  describe('pager', function(){
+    it('does not show pager on initialisation', function(){
+      expect(scope.showPager).toEqual(false);
+    });
 
-    var results = [];
+    it('opens pager when we have more results to show', function(){
+      scope.queryFromForm = 'hello';
 
-    deferred.resolve(results);
+      var deferred = $q.defer();
 
-    spyOn(IMS, 'search').and.returnValue(deferred.promise);
+      var results = { lastPage: false, items: []};
 
-    scope.search();
+      deferred.resolve({data: results});
 
-    expect(IMS.search).toHaveBeenCalled();
+      spyOn(IMS, 'search').and.returnValue(deferred.promise);
+
+      scope.search();
+
+      scope.$apply();
+
+      expect(scope.showPager).toBe(true);
+    });
+
+    it('has a method to handle moving forward in the results', function(){
+      expect(typeof(scope.nextPage)).toBe('function');
+    });
+
+    function repeat(){ var x=[]; var i=1; while(x.push(i++) < 50); return x; }
+
+    describe('next page', function(){
+      it('calls IMS for the next page', function(){
+        scope.queryFromForm = 'hello world';
+        scope.results = {items: repeat(), lastPage: false};
+        scope.totalResults = scope.results.items.length;
+
+        var deferred = $q.defer();
+
+        var results = { lastPage: false, items: repeat()};
+
+        deferred.resolve({data: results});
+
+        spyOn(IMS, 'search').and.returnValue(deferred.promise);
+
+        scope.nextPage();
+
+        scope.$apply();
+
+        expect(IMS.search).toHaveBeenCalledWith('hello world', 50, 50);
+
+        scope.nextPage();
+
+        scope.$apply();
+
+        expect(IMS.search.calls.argsFor(1)).toEqual(['hello world', 50, 100]);
+
+        scope.nextPage();
+
+        scope.$apply();
+
+        expect(IMS.search.calls.argsFor(2)).toEqual(['hello world', 50, 150]);
+      });
+
+      it('shows the previous page button correctly');
+      it('shows the next page button correctly');
+      it('gracefully handles errors in network call');
+    });
+
+    describe('previous page', function(){
+      it('calls IMS for the previous page');
+      it('shows the previous page button correctly');
+      it('shows the next page button correctly');
+      it('gracefully handles errors in network call');
+    });
   });
 
-  it('calls IMS with specified query', function(){
-    var query = 'hello world';
-    scope.queryFromForm = query;
+  describe('network operations', function(){
+    it('calls the IMS service', function(){
+      scope.queryFromForm = 'hello world';
+      var deferred = $q.defer();
 
-    var deferred = $q.defer();
+      var results = [];
 
-    var results = { items: [], lastPage: true};
+      deferred.resolve(results);
 
-    deferred.resolve(results);
+      spyOn(IMS, 'search').and.returnValue(deferred.promise);
 
-    spyOn(IMS, 'search').and.returnValue(deferred.promise);
+      scope.search();
 
-    scope.search();
+      expect(IMS.search).toHaveBeenCalled();
+    });
 
-    expect(IMS.search).toHaveBeenCalledWith(query);
+    it('calls IMS with specified query', function(){
+      var query = 'hello world';
+      scope.queryFromForm = query;
+
+      var deferred = $q.defer();
+
+      deferred.resolve({});
+
+      spyOn(IMS, 'search').and.returnValue(deferred.promise);
+
+      scope.search();
+
+      expect(IMS.search).toHaveBeenCalledWith(query);
+    });
+
+    it('sets scope variable correctly', function(){
+      scope.queryFromForm = 'hello world';
+
+      var deferred = $q.defer();
+      var response = { items: [], lastPage: true};
+
+      deferred.resolve({data: response});
+
+      spyOn(IMS, 'search').and.returnValue(deferred.promise);
+
+      scope.search();
+
+      scope.$apply();
+
+      expect(scope.results).toEqual(response);
+    });
+
+    it('has no errors when initialising the controller', function(){
+      scope.$apply();
+      expect(scope.errors.length).toEqual(0);
+    });
+
+    it('handles errors from the service', function(){
+      scope.queryFromForm = 'a book thing that does not exist';
+
+      var deferred = $q.defer();
+      var error = {message: 'Something went sadly wrong.'};
+      deferred.reject({message: 'Something went sadly wrong.'});
+
+      spyOn(IMS, 'search').and.returnValue(deferred.promise);
+
+      scope.search();
+
+      scope.$apply();
+
+      expect(scope.errors.length).toEqual(1);
+      expect(scope.errors[0]).toEqual(error);
+    });
+
+    it('clears errors before making call to IMS service', function(){
+      scope.queryFromForm = 'a book thing that does not exist';
+
+      // Shunt something onto errors that we are going to clear.
+      scope.errors.push({message: 'This is an error we will clear in scope.search'});
+
+      var deferred = $q.defer();
+      var error = {message: 'Something went sadly wrong.'};
+      deferred.reject(error);
+
+      spyOn(IMS, 'search').and.returnValue(deferred.promise);
+
+      expect(scope.errors.length).toEqual(1);
+
+      scope.search();
+
+      expect(scope.errors.length).toEqual(0);
+
+      // Now resolve the promise to double check we are covering the right bit of code.
+      scope.$apply();
+
+      expect(scope.errors.length).toEqual(1);
+      expect(scope.errors[0]).toEqual(error);
+    });
   });
 
-  it('sets scope variable correctly', function(){
-    scope.queryFromForm = 'hello world';
 
-    var deferred = $q.defer();
-    var response = { items: [], lastPage: true};
-
-    deferred.resolve({data: response});
-
-    spyOn(IMS, 'search').and.returnValue(deferred.promise);
-
-    scope.search();
-
-    scope.$apply();
-
-    expect(scope.results).toEqual(response);
-  });
-
-  it('has no errors when initialising the controller', function(){
-    scope.$apply();
-    expect(scope.errors.length).toEqual(0);
-  });
-
-  it('handles errors from the service', function(){
-    scope.queryFromForm = 'a book thing that does not exist';
-
-    var deferred = $q.defer();
-    var error = {message: 'Something went sadly wrong.'};
-    deferred.reject({message: 'Something went sadly wrong.'});
-
-    spyOn(IMS, 'search').and.returnValue(deferred.promise);
-
-    scope.search();
-
-    scope.$apply();
-
-    expect(scope.errors.length).toEqual(1);
-    expect(scope.errors[0]).toEqual(error);
-  });
-
-  it('clears errors before making call to IMS service', function(){
-    scope.queryFromForm = 'a book thing that does not exist';
-
-    // Shunt something onto errors that we are going to clear.
-    scope.errors.push({message: 'This is an error we will clear in scope.search'});
-
-    var deferred = $q.defer();
-    var error = {message: 'Something went sadly wrong.'};
-    deferred.reject(error);
-
-    spyOn(IMS, 'search').and.returnValue(deferred.promise);
-
-    expect(scope.errors.length).toEqual(1);
-
-    scope.search();
-
-    expect(scope.errors.length).toEqual(0);
-
-    // Now resolve the promise to double check we are covering the right bit of code.
-    scope.$apply();
-
-    expect(scope.errors.length).toEqual(1);
-    expect(scope.errors[0]).toEqual(error);
-  });
 });
